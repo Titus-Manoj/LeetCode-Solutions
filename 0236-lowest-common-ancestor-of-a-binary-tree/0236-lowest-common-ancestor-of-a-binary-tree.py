37a942476c9295f3970c6f7f34f468d1cd178b4f
Titus Manoj <56962660+Titus-Manoj@users.noreply.github.com>
2025-03-07 18:05:27 -0500

    Time: 59 ms (29.89%), Space: 22.2 MB (21.6%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,40 +6,13 @@
 #         self.right = None
 
 class Solution:
-    def findpath(self, root, n):
-        def recur(root, pos, n):
-            if root == None:
-                return None
-            pos.append(root.val)
-            if root.val == n:
-                return pos[:]
-                
-            left = recur(root.left, pos, n)
-            if left:
-                return left
-            right = recur(root.right, pos, n)
-            if right:
-                return right
-            pos.remove(root.val)
-            return None
-        return recur(root, [], n) or []
-            
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        ppath = self.findpath(root, p.val)
-        qpath = self.findpath(root, q.val)
-        lca = None
-        for i in ppath:
-            for j in qpath:
-                #print(str(i)+" "+str(j))
-                if i == j:
-                    lca = i
-        def find_node(root, val):
-            if not root:
-                return None
-            if root.val == val:
-                return root
-            return find_node(root.left, val) or find_node(root.right, val)
-
-        return find_node(root, lca)
+        if root == None or root == p or root == q:
+            return root
         
-        return ans
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        return left or right
