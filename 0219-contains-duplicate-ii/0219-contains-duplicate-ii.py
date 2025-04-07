4062850971ff7b642301347255430ad37bbe6804
Titus Manoj <56962660+Titus-Manoj@users.noreply.github.com>
2025-04-07 16:48:01 -0400

    Time: 31 ms (29.14%), Space: 36.6 MB (12.83%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,19 +1,12 @@
-class Solution(object):
-    def containsNearbyDuplicate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
-        window = set()
-        left = 0
-
-        for right in range(len(nums)):
-            if right - left > k:
-                window.remove(nums[left])
-                left += 1
-            if nums[right] in window:
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # Create hset for storing previous of k elements...
+        hset = {}
+        # Traverse for all elements of the given array in a for loop...
+        for idx in range(len(nums)):
+            # If duplicate element is present at distance less than equal to k, return true...
+            if nums[idx] in hset and abs(idx - hset[nums[idx]]) <= k:
                 return True
-            window.add(nums[right])
-        
+            hset[nums[idx]] = idx
+        # If no duplicate element is found then return false...
         return False
