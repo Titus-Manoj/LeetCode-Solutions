f272d6e074800704826fcc7ea3cc54b37d3706c2
Titus Manoj <56962660+Titus-Manoj@users.noreply.github.com>
2025-04-11 15:46:58 -0400

    Time: 853 ms (24.28%), Space: 18 MB (13.06%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.py
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.py
@@ -1,17 +1,16 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
-        count = 0
-        for num in range(low, high+1):
+        def digits_count(n):
+            return len(str(n))
+        
+        res = 0
+        for num in range(low, high + 1):
             s = str(num)
-            n = len(s)
-            if n%2 == 0:
-                first = s[:n//2]
-                last = s[n//2:]
-                sumf = 0
-                suml = 0
-                for i in range(n//2):
-                    sumf += int(first[i])
-                    suml += int(last[i])
-                if sumf == suml:
-                    count += 1
-        return count
+            if len(s) % 2 != 0:
+                continue
+            mid = len(s) // 2
+            left_sum = sum(int(d) for d in s[:mid])
+            right_sum = sum(int(d) for d in s[mid:])
+            if left_sum == right_sum:
+                res += 1
+        return res
