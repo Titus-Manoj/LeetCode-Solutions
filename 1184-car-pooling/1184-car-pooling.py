1963fbc35bcfb2b9cff5b780f033dab6786ede73
Titus Manoj <56962660+Titus-Manoj@users.noreply.github.com>
2025-04-15 17:36:29 -0400

    Time: 3 ms (63.77%), Space: 18.2 MB (50.16%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.py
+++ 1184-car-pooling/1184-car-pooling.py
@@ -1,14 +1,13 @@
-import heapq
 class Solution:
-    def carPooling(self, tp: List[List[int]], cap: int) -> bool:
-        hp = []
-        jr = [0]*1000
-        for i in range(len(tp)):
-            f, t, ppl = tp[i][1], tp[i][2], tp[i][0]
-            for j in range(f-1, t-1):
-                jr[j] += ppl
-        
-        for i in range(1000):
-            if jr[i] > cap:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        lst = []
+        for n, start, end in trips:
+            lst.append((start, n))
+            lst.append((end, -n))
+        lst.sort()
+        pas = 0
+        for loc in lst:
+            pas += loc[1]
+            if pas > capacity:
                 return False
         return True
