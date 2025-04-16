56210bbd2d5d92973223dcd0f22a424803534af2
Titus Manoj <56962660+Titus-Manoj@users.noreply.github.com>
2025-04-16 15:36:17 -0400

    Time: 0 ms (100%), Space: 17.7 MB (49.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,8 +3,8 @@ class Solution:
         a = 1
         b = 1
         while n>0:
-            t = a+b
-            a = b
-            b = t
+            b = a+b
+            a = b-a
+            #b = t
             n-=1
         return a
