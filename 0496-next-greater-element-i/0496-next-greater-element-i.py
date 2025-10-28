f8d3167e44f2259b6e36a93b10b7a060f9d9220e
Titus Manoj <56962660+Titus-Manoj@users.noreply.github.com>
2025-10-28 16:17:11 -0400

    Time: 0 ms (100%), Space: 18.2 MB (20.7%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hp = {}
+        stk = []
+        for i in reversed(nums2):
+            while stk and stk[-1] <= i:
+                stk.pop()
+            hp[i] = -1 if not stk else stk[-1]
+            stk.append(i)
+        
+        return [hp[num] for num in nums1]
