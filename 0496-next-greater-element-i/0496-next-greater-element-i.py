9c6f484b865965b10c4d431932febb8998cb9e26
Titus Manoj <56962660+Titus-Manoj@users.noreply.github.com>
2026-01-28 17:10:48 -0500

    Time: 0 ms (100%), Space: 19.7 MB (13.72%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,7 +1,8 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hp = {}
+        hp = defaultdict(lambda: -1)
         stk = []
+
         for i in reversed(nums2):
             while stk and stk[-1] <= i:
                 stk.pop()
